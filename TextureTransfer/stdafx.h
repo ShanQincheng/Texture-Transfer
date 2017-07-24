86bf249a4890c4ce53f62e298049d720ec824feb
codingstory <codingstory.tw@gmail.com>
2017-07-24 21:13:20 +0800

    delete useless headfiles

--- TextureTransfer/stdafx.h
+++ TextureTransfer/stdafx.h
@@ -24,14 +24,11 @@ typedef struct textureFingerprintStruct
 	int coordinate_y = 0;
 
 	//char fingerprint[64];
-	double fingerprint = 0.0;
+	//double fingerprint = 0.0;
+	unsigned long long int fingerprint = 0;
 }TextureFingerprintStruct;
 
 Mat SearchForSimilarAreas(Mat currentPatch, Mat texturePic);
 Mat Vertical(Mat rock1_, Mat rock2_, int cutRows);
 Mat Horizontal(Mat rock1_, Mat rock2_, int cutCols);
-TextureFingerprintStruct** GenerateTextureFingerprint(Mat texturePic, int patchRows, int patchCols);
-bool compare_fingerprint(TextureFingerprintStruct* const struct1, TextureFingerprintStruct* const struct2);
-
-
 // TODO: reference additional headers your program requires here
